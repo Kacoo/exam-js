31763d11f1f14260ee7e885d67e7d0624df142e3
kacoo <kacoo1205@qq.com>
2019-05-13 10:20:53 +0800

    leetcode: remove-element

---
+++ test/remove-element.js
@@ -0,0 +1,10 @@
+var removeElement = function (nums, val) {
+    for (var i = 0; i < nums.length;) {
+        if (val == nums[i]) {
+            nums.splice(i, 1);
+        } else i++;
+    }
+    return nums.length + nums;
+};
+
+alert(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
