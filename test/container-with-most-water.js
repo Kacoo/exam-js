28c9e52ac08ea7a7862e6a0c1f18b2eaf620d424
kacoo <kacoo1205@qq.com>
2019-05-27 21:03:09 +0800

    Leetcode: normal submit

--- test/container-with-most-water.js
+++ test/container-with-most-water.js
@@ -1,4 +1,20 @@
 // https://leetcode.com/problems/container-with-most-water/
-var maxArea = function(height) {
-    
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    var area = 0;
+    for (var i = height.length - 1; i >= 0; i--) {
+        var j = i - 1;
+        while (height[j] > -1) {
+            var temp = (i - j) * Math.min(height[i], height[j]);
+            area = temp > area ? temp : area;
+            j--;
+        }
+    }
+    return area;
 };
+
+var a = maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]);
+console.log(a);
---
+++ test/divide-two-integers.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/divide-two-integers/
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function (dividend, divisor) {
+    // 判断符号位
+    var flag = 0 == ((dividend ^ divisor) >> 31) ? true : false;
+
+    var count = 0;
+    dividend = -Math.abs(dividend);
+    divisor = Math.abs(divisor);
+    count = add(dividend, divisor, count);
+    if (count == 2147483648 && flag) return 2147483647;
+    return 0 == count ? 0 : flag ? count : -count;
+};
+
+function add(a, b, c) {
+    return a + b > 0 ? c : arguments.callee(a + b, b, ++c);
+}
+
+console.log(divide(2147483647, 3));
